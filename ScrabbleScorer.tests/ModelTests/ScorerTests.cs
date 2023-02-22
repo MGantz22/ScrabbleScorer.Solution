875cd1f3e420422700a5bccb073fa268b001f36a
Mitchell Gantz <mgantz22@icloud.com>
2023-02-22 10:16:40 -0800

    Score constructor passing test Co-authored-by: Eliot Gronstal <eliot.lauren@gmail.com> Co-authored-by: Hyung Jin Kim <hjkim971205@users.noreply.github.com> Co-authored-by: John Lenz <john.lenz27@gmail.com>

---
+++ ScrabbleScorer.tests/ModelTests/ScorerTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScrabbleScorer.Models;
+using System.Collections.Generic; 
+using System;
+
+namespace ScrabbleScorer.Tests
+{
+  [TestClass]
+  public class Scorer
+  {
+  [TestMethod]
+  public void ScorerConstructor_CreateInstanceofScorer_Scorer()
+  {
+    Scorer newScorer = new Scorer();
+    Assert.AreEqual(typeof(Scorer), newScorer.GetType());
+  }
+
+
+  }
+}
